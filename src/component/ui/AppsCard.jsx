60939dc0aa953fa4e7e4e9618eac8afc39758e36
antick-arch <e241003@ugrad.iiuc.ac.bd>
2026-09-23 12:39:08 +0700

    refactor: enhance AppsCard component layout and remove unnecessary comments

--- src/component/ui/AppsCard.jsx
+++ src/component/ui/AppsCard.jsx
@@ -4,8 +4,9 @@ import { FaDownload, FaStar } from "react-icons/fa";
 const AppsCard = ({app}) => {
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
-      <figure>
+      <figure className="p-5">
         <img
+        className="w-50"
           src={app.image}
           alt={app.title}
         />
@@ -13,12 +14,7 @@ const AppsCard = ({app}) => {
       <div className="card-body">
         <h2 className="card-title">
           {app.title}
-          <div className="badge badge-secondary">NEW</div>
         </h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
         <div className="card-actions justify-between font-bold">
           <div className="badge badge-outline border-none bg-green-100 text-green-500 py-4 px-8"><FaDownload /> {app.downloads}</div>
           <div className="badge badge-outline border-none bg-orange-100 text-orange-500 py-4 px-8"><FaStar /> {app.ratingAvg}</div>

9be05faa12a2153653ebb143fd6e60d94d6798b8
antick-arch <e241003@ugrad.iiuc.ac.bd>
2026-09-23 21:45:40 +0700

    feat: implement data fetching in TrendingApps component and add loading spinner

--- src/component/TrendingApps.jsx
+++ src/component/TrendingApps.jsx
@@ -1,10 +1,26 @@
-import React, { use } from "react";
+import React, { use, useEffect, useState } from "react";
 import { CgLayoutGrid } from "react-icons/cg";
 import AppsCard from "./ui/AppsCard";
+import { BallTriangle } from "react-loader-spinner";
 
-const TrendingApps = ({ appsPromise }) => {
-  const appData = use(appsPromise);
+const TrendingApps = () => {
+  //   const appData = use(appsPromise);
   // console.log(appData);
+  const [loader, setLoader] = useState(true);
+  const [appData, setAppData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/data.json");
+      const data = await res.json();
+      console.log(data);
+      setTimeout(() => {
+        setAppData(data);
+        setLoader(false);
+      },1000);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="my-10 container mx-auto space-y-10">
       <div className="text-center">
@@ -16,9 +32,22 @@ const TrendingApps = ({ appsPromise }) => {
         </p>
       </div>
       <div className="grid grid-cols-3 gap-5">
-        {appData.map((app) => (
-          <AppsCard app={app} key={app.id}></AppsCard>
-        ))}
+        {loader ? (
+          <div className="container mx-auto col-span-3 flex justify-center items-center">
+            <BallTriangle
+              height={100}
+              width={100}
+              radius={5}
+              color="#4fa94d"
+              ariaLabel="ball-triangle-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+          </div>
+        ) : (
+          appData.map((app) => <AppsCard app={app} key={app.id}></AppsCard>)
+        )}
       </div>
     </div>
   );
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,18 +1,14 @@
-import React, { Suspense } from "react";
 import Banner from "../../component/Banner";
 import Stats from "../../component/Stats";
 import TrendingApps from "../../component/TrendingApps";
 
-const appsPromise = fetch("/data.json").then((res) => res.json());
 
 const HomePage = () => {
   return (
     <div>
       <Banner></Banner>
       <Stats></Stats>
-      <Suspense fallback={<div className="flex justify-center p-3"><span className="loading loading-bars loading-xl"></span></div>}>
-        <TrendingApps appsPromise={appsPromise}></TrendingApps>
-      </Suspense>
+      <TrendingApps></TrendingApps>
     </div>
   );
 };
